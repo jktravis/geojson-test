e10569e6a89795b6a69a34ac12de5763523eb1bf
jktravis <joshua.travis@gmail.com>
2018-12-13 13:30:24 -0500

    add the things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, createRef } from 'react';
 import './App.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { fromLonLat } from "ol/proj.js";
+import OSM from 'ol/source/OSM';
+import GeoJSON from 'ol/format/GeoJSON';
+import { getGeoJson, styleFunction } from './map';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.map = new Map();
+    this.mapDomRef = createRef();
+  }
+
+  componentDidMount() {
+    this.mapView = new View({
+      center: [0,0],
+      zoom: 2,
+    });
+
+    this.map.addLayer(new TileLayer({
+        source: new OSM(),
+      }),
+    );
+
+    this.map.setTarget(this.mapDomRef.current);
+    this.map.setView(this.mapView);
+
+    this.loadLayers();
+  }
+
+  loadLayers = () => {
+    getGeoJson()
+      .then(data => {
+        this.vectorSource = new VectorSource({
+          features: (new GeoJSON().readFeatures(data))
+        });
+
+        this.vectorLayer = new VectorLayer({
+          source: this.vectorSource,
+          style: styleFunction
+        })
+        this.map.addLayer(this.vectorLayer)
+      })
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div ref={this.mapDomRef}/>
       </div>
     );
   }
